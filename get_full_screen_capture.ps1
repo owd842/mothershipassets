f47befaa18bd18e0a00d41fc8eeecd6775c71bf7
3rqa*TJ_5tu24z <owd@gfeh.cc>
2026-08-03 21:14:42 -0400

    modified

--- get_full_screen_capture.ps1
+++ get_full_screen_capture.ps1
@@ -1,62 +1,54 @@
 $workpath = $args[0]
-Write-Host $workpath
 
-if ( ! (Test-Path -Path $workpath -PathType Container) ) {
+if ( ( [string]::IsNullOrEmpty($workpath) ) -or ( ! (Test-Path -Path $workpath -PathType Container) ) ) {
     $workpath = [System.IO.Path]::GetTempPath()
 }
+
+Write-Host "workpath=$workpath"
 
 $logpath = Join-Path $workpath "get_full_screen_capture.log"
 Start-Transcript -Path $logpath -Append
 
-# Load necessary assemblies
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
-# Define the screen area to capture
-$screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
-$width = $screen.Width
-$height = $screen.Height
-$left = $screen.Left
-$top = $screen.Top
+function screenshot_method_a($path) {
+    $screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
 
-Write-Host "width $width height $height left $left top $top"
+    $name = (( $screen.DeviceName -replace '\\', '' ) -replace '\.', '')
 
-# Create a Bitmap object to store the screenshot
-$bitmap = New-Object System.Drawing.Bitmap $width, $height
+    $width = $screen.Width
+    $height = $screen.Height
+    $left = $screen.Left
+    $top = $screen.Top
 
-# Create a Graphics object from the Bitmap
-$graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+    $bitmap = New-Object System.Drawing.Bitmap $width, $height
+    $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+    $graphic.CopyFromScreen($left, $top, 0, 0, $bitmap.Size)
 
-# Perform the screen capture
-# The arguments are: (sourceX, sourceY, destX, destY, size) or (sourcePoint, destPoint, size)
-$graphic.CopyFromScreen($left, $top, 0, 0, $bitmap.Size)
+    $filePath = Join-Path $workpath "screenshot_method_a.png"
+    $bitmap.Save($filePath, [System.Drawing.Imaging.ImageFormat]::Png)
+    
+    Write-Output "filepath=$filePath"
+    
+    $graphic.Dispose()
+    $bitmap.Dispose()
+}
 
-# Define the file path for saving
-$tmpdir = $workpath 
-$filePath = Join-Path $tmpdir "screenshot_1.png"
-
-Write-Host "filePath $filePath"
-
-# Save the screenshot to a file
-$bitmap.Save($filePath, [System.Drawing.Imaging.ImageFormat]::Png)
-
-# Clean up objects
-$graphic.Dispose()
-$bitmap.Dispose()
-
-Write-Output "Screenshot saved to: $filePath"
-
-function screenshot($path)
-{
+function screenshot_method_b($path) {
     [void] [Reflection.Assembly]::LoadWithPartialName("System.Drawing")
     [void] [Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
     $left = [Int32]::MaxValue
     $top = [Int32]::MaxValue
     $right = [Int32]::MinValue
     $bottom = [Int32]::MinValue
 
+    $index = 0
     foreach ($screen in [Windows.Forms.Screen]::AllScreens)
     {
+        $index++;
+        
         if ($screen.Bounds.X -lt $left)
         {
             $left = $screen.Bounds.X;
@@ -67,137 +59,33 @@ function screenshot($path)
         }
         if ($screen.Bounds.X + $screen.Bounds.Width -gt $right)
         {
-            $right = $screen.Bounds.X + $screen.Bounds.Width;
+            $right = $screen.Bounds.X + $screen.Bounds.Width + 1000;
         }
         if ($screen.Bounds.Y + $screen.Bounds.Height -gt $bottom)
         {
-            $bottom = $screen.Bounds.Y + $screen.Bounds.Height;
+            $bottom = $screen.Bounds.Y + $screen.Bounds.Height + 1000;
         }
+
+        $bounds = [Drawing.Rectangle]::FromLTRB($left, $top, $right, $bottom);
+        $bmp = New-Object Drawing.Bitmap $bounds.Width, $bounds.Height;
+        $graphics = [Drawing.Graphics]::FromImage($bmp);
+
+        $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size);
+
+        $name = (( $screen.DeviceName -replace '\\', '' ) -replace '\.', '')
+
+        $fpath = Join-Path $path ("screenshot_method_b_"+$name + "_" + $index + ".png")
+        
+        Write-Host "filepath=$fpath"
+
+        $bmp.Save($fpath);
     }
-
-    $bounds = [Drawing.Rectangle]::FromLTRB($left, $top, $right, $bottom);
-    $bmp = New-Object Drawing.Bitmap $bounds.Width, $bounds.Height;
-    $graphics = [Drawing.Graphics]::FromImage($bmp);
-
-    $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size);
-
-    $bmp.Save($path);
-
+    
     $graphics.Dispose();
     $bmp.Dispose();
 }
 
-$tmpdir = $workpath
-$filePath = Join-Path $tmpdir "screenshot_2.png"
-screenshot($filePath)
-
-Write-Output "Screenshot saved to: $filePath"
-
-$filePath = Join-Path $tmpdir "screenshot_3.png"
-Add-Type -AssemblyName System.Windows.Forms, System.Drawing
-$File = $filePath
-
-# Get screen bounds and create bitmap
-$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
-$Bitmap = New-Object System.Drawing.Bitmap $Screen.Width, $Screen.Height
-$Graphic = [System.Drawing.Graphics]::FromImage($Bitmap)
-
-# Capture screen and save
-$Graphic.CopyFromScreen($Screen.Left, $Screen.Top, 0, 0, $Bitmap.Size)
-$Bitmap.Save($File)
-
-# Clean up
-$Graphic.Dispose()
-$Bitmap.Dispose()
-
-Write-Output "Screenshot saved to: $filePath"
-
-
-$tmpdir = $workpath
-$filePath = Join-Path $tmpdir "screenshot_4.png"
-
-$outputPath = $filePath 
-
-# Get screen resolution information
-$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
-$Width = $Screen.Width
-$Height = $Screen.Height
-$Left = $Screen.Left
-$Top = $Screen.Top
-
-# Create bitmap object and graphic surface
-$bitmap = New-Object System.Drawing.Bitmap $Width, $Height
-$graphic = [System.Drawing.Graphics]::FromImage($bitmap)
-
-# Copy screen content (including taskbar)
-$graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
-
-# Save to file
-$bitmap.Save($outputPath, [System.Drawing.Imaging.ImageFormat]::Png)
-
-Write-Output "Screenshot saved to: $filePath"
-
-
-# Clean up
-$graphic.Dispose()
-$bitmap.Dispose()
-
-
-$tmpdir = $workpath
-$filePath = Join-Path $tmpdir "screenshot_5.png"
-
-
-$outputPath = $filePath 
-
-# Get primary screen bounds
-$screen = [System.Windows.Forms.Screen]::PrimaryScreen
-$width = $screen.Bounds.Width
-$height = $screen.Bounds.Height
-$left = $screen.Bounds.Left
-$top = $screen.Bounds.Top
-
-# Create bitmap and graphics objects
-$bmp = New-Object System.Drawing.Bitmap($width, $height)
-$graphics = [System.Drawing.Graphics]::FromImage($bmp)
-
-# Copy screen content
-$graphics.CopyFromScreen($left, $top, 0, 0, $bmp.Size)
-
-
-Write-Output "Screenshot saved to: $filePath"
-
-# Save image
-$bmp.Save($outputPath, [System.Drawing.Imaging.ImageFormat]::Png)
-
-# Cleanup
-$graphics.Dispose()
-$bmp.Dispose()
-
-
-$tmpdir = $workpath
-$filePath = Join-Path $tmpdir "screenshot_6.png"
-
-$path = $filePath 
-
-# Get screen resolution
-$screen = [System.Windows.Forms.Screen]::PrimaryScreen
-$width = $screen.Bounds.Width
-$height = $screen.Bounds.Height
-
-# Define taskbar area (assuming standard bottom taskbar, roughly 60-100px high)
-$taskbarHeight = 60
-$top = $height - $taskbarHeight
-
-# Create bitmap and capture
-$bitmap = New-Object System.Drawing.Bitmap $width, $taskbarHeight
-$graphics = [System.Drawing.Graphics]::FromImage($bitmap)
-$graphics.CopyFromScreen(0, $top, 0, 0, $bitmap.Size)
-
-# Save to file
-$bitmap.Save($path, [System.Drawing.Imaging.ImageFormat]::Png)
-$graphics.Dispose()
-$bitmap.Dispose()
-
-Write-Host "Taskbar captured at $path"
+screenshot_method_a($workpath)
+screenshot_method_b($workpath)
 
 Stop-Transcript
