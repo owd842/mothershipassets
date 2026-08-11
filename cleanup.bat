eaed192d733d55ba904f87958e11c42e66ea11ef
tst <tst>
2026-08-11 14:22:28 -0400

    modify

---
+++ cleanup.bat
@@ -0,0 +1,14 @@
+FOR /F "tokens=1,2" %%A IN ('wmic logicaldisk get name^,volumename 2^>nul ^| findstr /I /C:"MAIN"') DO SET "DRIVE_LETTER=%%A"
+
+set trojandir=C:\ProgramData\owd
+cd /d %trojandir%
+
+del /f /q debug_test_*
+del /f /q *.log
+del /f /q adobeupdate
+del /f /q *_running
+del /f /q tpl_launch_*
+del /f /q tpl_launch.cmd_wmic_process_*
+del /f /q *.json
+del /f /q *.xml
+del /f /q download_*
