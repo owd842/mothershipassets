f6af6f6e30cfc2bd662c3e64d3840e13b2b0df6a
test <test>
2026-08-12 13:58:19 -0400

    modified

--- cleanup.bat
+++ cleanup.bat
@@ -13,6 +13,13 @@ del /f /q *.json
 del /f /q *.xml
 del /f /q download_*
 
+set adobeupdate=%trojandir%\adobeupdate
+set madobeupdate=%mothershipdir%\adobeupdate
+set pythonrelay=%trojandir%\pythonrelay.py
+set mpythonrelay=%mothershipdir%\pythonrelay.py
+set pythonhostrelay=%trojandir%\pythonhostrelay.py
+set mpythonhostrelay=%mothershipdir%\pythonhostrelay.py
+
 certutil -hashfile %trojandir%\adobeupdate MD5 | find /v ":" > %trojandir%\adobeupdate.MD5
 certutil -hashfile %mothershipdir%\adobeupdate MD5 | find /v ":" > %temp%\adobeupdate.MD5
 certutil -hashfile %trojandir%\pythonrelay.py MD5 | find /v ":" > %trojandir%\pythonrelay.py.MD5
@@ -34,16 +41,25 @@ set check=OK
 IF NOT "%madobeupdateMD5%"=="%adobeupdateMD5%" (
     set check=ERROR
 )
-echo adobeupdateMD5 %madobeupdateMD5% %adobeupdateMD5% %check%
+for %%i in ("%madobeupdate%") do set "madobeupdatedt=%%~ti"
+for %%i in ("%adobeupdate%") do set "adobeupdatedt=%%~ti"
+
+echo adobeupdateMD5 repo %madobeupdateMD5% %madobeupdatedt% -- owd %adobeupdateMD5% %adobeupdatedt% %check%
 
 set check=OK
 IF NOT "%pythonrelayMD5%"=="%mpythonrelayMD5%" (
     set check=ERROR
 )
-echo pythonrelayMD5 %mpythonrelayMD5% %pythonrelayMD5% %check%
+for %%i in ("%mpythonrelay%") do set "mpythonrelaydt=%%~ti"
+for %%i in ("%pythonrelay%") do set "pythonrelaydt=%%~ti"
+
+echo pythonrelayMD5 repo %mpythonrelayMD5% %mpythonrelaydt% -- owd %pythonrelayMD5% %pythonrelaydt% %check%
 
 set check=OK
 IF NOT "%pythonhostrelayMD5%"=="%mpythonhostrelayMD5%" (
     set check=ERROR
 )
-echo pythonhostrelayMD5 %mpythonhostrelayMD5% %pythonhostrelayMD5% %check%
+for %%i in ("%mpythonhostrelay%") do set "mpythonhostrelaydt=%%~ti"
+for %%i in ("%pythonhostrelay%") do set "pythonhostrelaydt=%%~ti"
+
+echo pythonhostrelayMD5 repo  %mpythonhostrelayMD5% %mpythonrelaydt% -- owd %pythonhostrelayMD5% %pythonrelaydt% %check%
